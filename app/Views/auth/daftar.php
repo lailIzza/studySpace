2c2e4722c97064c0af2c6823c919d53bb25abb1a
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-07-04 23:48:34 +0700

    halaman daftar

---
+++ app/Views/auth/daftar.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Study Space</title>
+</head>
+<body>
+    <h2>Formulir Daftar</h2>
+</body>
+</html>
