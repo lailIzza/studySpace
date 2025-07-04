845c9cea13b894f740f6f3c866603647b3b8e21c
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-07-04 23:49:08 +0700

    halaman beranda

---
+++ app/Views/postingan/beranda.php
@@ -0,0 +1,9 @@
+<?= $this->section('style') ?>
+    <link rel="stylesheet" href="<?= base_url('css/beranda.css') ?>">
+<?= $this->endSection() ?>
+
+<?= $this->extend('layout/template')?>
+
+<?= $this->section('content')?>
+ <h1>Ini Beranda</h1>
+<?= $this->endSection()?>
