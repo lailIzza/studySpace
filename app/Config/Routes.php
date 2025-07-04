a2e95f2cc24ce81d6573bade7f31439ca15b231e
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-07-04 23:46:55 +0700

    router

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,17 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+//$routes->get('/', 'Home::index');
+
+//router beranda,buat,detail,pengumuman
+$routes->get('/', 'Postingan::beranda');
+$routes->get('/buat', 'Postingan::buat');
+$routes->get('/detail', 'Postingan::detail');
+$routes->get('/pengumuman', 'Postingan::pengumuman');
+
+//routes user profil
+$routes->get('/profil', 'User::profil');
+
+//routes auth
+$routes->get('/login', 'Auth::login');
+$routes->get('/daftar', 'Auth::daftar');
