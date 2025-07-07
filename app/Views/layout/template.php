9c9735ca37797b9ef91a9e07a9d8c52431f8ac93
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-07-07 20:14:17 +0700

    update navbar

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" 
     crossorigin="anonymous">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
     <title><?= $title ?? '' ?></title>
     <link rel="stylesheet" href="/css/template.css">
     <?= $this->renderSection('style') ?>
@@ -14,15 +15,13 @@
 
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg navbar-custom px-3 py-2">
-        <div class="container-fluid">
+        <div class="container">
             <a class="navbar-brand fw-bold" href="/">StudySpace</a>
 
             <div class="collapse navbar-collapse" id="navbarContent">
-                <!-- Tombol Cari -->
                 <form class="d-flex mx-auto my-2 my-lg-0" style="width: 50%;">
                     <input class="form-control me-2 rounded-pill" type="search" placeholder="Cari pertanyaan..." aria-label="Search">
                 </form>
-                <!-- Menu -->
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link" href="buat">Ajukan Pertanyaan</a>
@@ -31,7 +30,6 @@
                         <a class="nav-link" href="pengumuman">Pengumuman</a>
                     </li>
                 </ul>
-                <!-- Tombol -->
                 <div class="d-flex gap-2">
                     <a href="daftar" class="btn btn-daftar">Daftar</a>
                     <a href="login" class="btn btn-login">Login</a>
