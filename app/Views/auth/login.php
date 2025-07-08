bff654435ea2d3a03b8356a79dbb529a4ccc5976
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-07-09 00:53:53 +0700

    tambah logika login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -19,7 +19,7 @@
       <a href="<?= base_url('daftar') ?>" class="btn-outline-custom">Daftar</a>
 
       <div class="mt-auto align-self-end">
-        <img src="https://www.jotform.com/blog/wp-content/uploads/2025/04/image-64.png" alt="Ilustrasi" class="img-fluid" style="max-width: 320px;" />
+        <img src="<?= base_url('asset/ilustrasi_login.png') ?>" alt="Ilustrasi" class="img-fluid" />
       </div>
     </div>
 
@@ -27,15 +27,29 @@
     <div class="right-side d-flex align-items-center justify-content-center" style="flex: 2.55; background-color: #ffffff;">
       <div class="form-box bg-white p-5 rounded shadow" style="max-width: 600px; width: 100%;">
         <h2 class="text-center fw-bold mb-4" style="color: #2d1460;">Login</h2>
-        <form>
-          <div class="input-wrapper d-flex align-items-center bg-light p-2 rounded mb-3">
-            <i class='bx bx-user me-2' style="color: #3f2d7c;"></i>
-            <input type="text" class="form-control border-0 bg-transparent" placeholder="Username" required />
+
+        <!-- flash data untuk pesan error -->
+        <?php if (session()->getFlashdata('error')): ?>
+          <div class="alert alert-danger">
+            <?= session()->getFlashdata('error') ?>
+          </div>
+        <?php endif; ?>
+
+        <?php if (session()->getFlashdata('success')): ?>
+          <div class="alert alert-success">
+            <?= session()->getFlashdata('success') ?>
+          </div>
+        <?php endif; ?>
+
+        <form action="<?= base_url('login/process') ?>" method="post">
+          <div class="input-wrapper d-flex align-items-center bg-light p-2 rounded mb-4">
+            <i class='bx bx-envelope me-2' style="color: #3f2d7c;"></i>
+            <input type="text" name="email" class="form-control border-0 bg-transparent" placeholder="E-mail" required />
           </div>
 
           <div class="input-wrapper d-flex align-items-center bg-light p-2 rounded mb-4">
             <i class='bx bx-lock-alt me-2' style="color: #3f2d7c;"></i>
-            <input type="password" class="form-control border-0 bg-transparent" placeholder="Password" required />
+            <input type="password" name="password" class="form-control border-0 bg-transparent" placeholder="Password" required />
           </div>
 
           <button type="submit" class="btn-filled-custom">Login</button>
