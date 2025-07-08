fc18e56ea94947192234adeb9b029df8ee7731c1
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-07-09 00:53:27 +0700

    tambah logika untuk daftar

--- app/Views/auth/daftar.php
+++ app/Views/auth/daftar.php
@@ -19,8 +19,7 @@
       <a href="<?= base_url('login') ?>" class="btn-outline-custom">Login</a>
 
       <div class="mt-auto align-self-end">
-        <img src="asset/ilustrasi_login.png"
-            alt="Ilustrasi" class="img-fluid" style="max-width: 320px;" />
+        <img src="<?= base_url('asset/ilustrasi_daftar.png') ?>" alt="Ilustrasi" class="img-fluid" />
       </div>
     </div>
 
@@ -28,16 +27,16 @@
     <div class="right-side d-flex align-items-center justify-content-center" style="flex: 2.55; background-color: #ffffff;">
       <div class="form-box bg-white p-5 rounded shadow" style="max-width: 600px; width: 100%;">
         <h2 class="text-center fw-bold mb-4" style="color: #2d1460;">Daftar</h2>
-        <form>
+        <form action="<?= base_url('register/process') ?>" method="post">
 
           <div class="input-wrapper d-flex align-items-center bg-light p-2 rounded mb-3">
             <i class='bx bx-user me-2' style="color: #3f2d7c;"></i>
-            <input type="text" class="form-control border-0 bg-transparent" placeholder="Username" />
+            <input type="text" name="username" class="form-control border-0 bg-transparent" placeholder="Username" />
           </div>
 
           <div class="input-wrapper d-flex align-items-center bg-light p-2 rounded mb-3">
             <i class='bx bx-envelope me-2' style="color: #3f2d7c;"></i>
-            <input type="email" class="form-control border-0 bg-transparent" placeholder="E-mail" required/>
+            <input type="email" name="email" class="form-control border-0 bg-transparent" placeholder="E-mail" required/>
           </div>
 
           <div class="mb-3">
@@ -45,8 +44,8 @@
               <i class='bx bx-user me-2' style="color: #3f2d7c;"></i>
               <select class="form-select border-0 bg-transparent shadow-none" style="font-size: 13px; font-weight: 500; color: #3f2d7c;" name="gender" required>
                 <option value="" disabled selected>Pilih Jenis Kelamin</option>
-                <option value="Laki-laki">Laki-laki</option>
-                <option value="Perempuan">Perempuan</option>
+                <option value="male">Laki-laki</option>
+                <option value="female">Perempuan</option>
               </select>
             </div>
           </div>
@@ -54,7 +53,7 @@
 
           <div class="input-wrapper d-flex align-items-center bg-light p-2 rounded mb-4">
             <i class='bx bx-lock-alt me-2' style="color: #3f2d7c;"></i>
-            <input type="password" class="form-control border-0 bg-transparent" placeholder="Password" />
+            <input type="password" name="password" class="form-control border-0 bg-transparent" placeholder="Password" />
           </div>
 
          <button type="submit" class="btn-filled-custom">Daftar</button>
