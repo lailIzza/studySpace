939237e064fea6c0b23d261f64f8e6e6723e44d6
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-07-15 20:34:16 +0700

    agar tampilan pertama user seuai role

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,18 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        if (session()->get('isLoggedIn')) {
+            if (session()->get('role') === 'admin') {
+                return redirect()->to(base_url('dashboard'));
+            } else {
+                return redirect()->to(base_url('beranda'));
+            }
+        }
+
+        // Panggil controller Postingan::beranda()
+        return redirect()->to(base_url('beranda'));
     }
+
 }
